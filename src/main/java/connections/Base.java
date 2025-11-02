1ed779efcbbcd959dc2e4f213a4295e88169b8cd
Serhii Kondriuchok <kondrserg@gmail.com>
2025-11-02 14:53:45 +0000

    Update Base.java

--- src/main/java/connections/Base.java
+++ src/main/java/connections/Base.java
@@ -1,51 +1,88 @@
 package connections;
 
-import staticconnections.StaticBase;
-
 public class Base {
     /*--------------Fields--------------*/
-    public String a = "public field: Base class";
-    protected String b = "protected field: Base class";
-    private String c = "private field: Base class";
-    String d = "package field: Base class";
+    public String a = "public field";
+    protected String b = "protected field";
+    private String c = "private field";
+    String d = "default field";
+    public static String e = "public static field";
+    protected static String f = "protected static field";
+    private static String g = "private static field";
+    static String h = "default static field";
     /*--------------Methods--------------*/
     public void sayA() {
-        System.out.println("public method: Base class");
+        System.out.println("public method");
     }
     protected void sayB() {
-        System.out.println("protected method: Base class");
+        System.out.println("protected method");
     }
     private void sayC() {
-        System.out.println("private method: Base class");
+        System.out.println("private method");
     }
     void sayD() {
-        System.out.println("package method: Base class");
+        System.out.println("default method");
+    }
+    public static void sayE() {
+        System.out.println("public static method");
+    }
+    protected static void sayF() {
+        System.out.println("protected static method");
+    }
+    private static void sayG() {
+        System.out.println("private static method");
+    }
+    static void sayH() {
+        System.out.println("default static method");
     }
     public static void main(String[] args) {
-        Base o = new Base();
-        Inner i = o.new Inner();
-        i.caller();
-        o.callStatic();
-    }
-    public void callStatic() {
-        System.out.println();
-        System.out.println(StaticBase.a); //access to public static field
-        System.out.println();
-        StaticBase.sayA(); //access to public static method
+        Inner i = new Base().new Inner();
+        i.call();
+        i.callStatic();
+        InnerStatic.callStatic();
     }
     /*--------------Inner Classes--------------*/
     public class Inner {
         /*--------------Methods--------------*/
-        public void caller() {
-            System.out.println(a); //access to public field
-            System.out.println(b); //access to protected field
-            System.out.println(c); //access to private field
-            System.out.println(d); //access to packege field
+        public void call() {
+            System.out.println("The INNER OBJECT has private, default, "
+                    + "protected and public access to the OUTER OBJECT");
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            sayA();
+            sayB();
+            sayC();
+            sayD();
+        }
+        public void callStatic() {
+            System.out.println("The INNER OBJECT has private static, default static, "
+                    + "protected static and public static access to the OUTER CLASS");
+            System.out.println(e);
+            System.out.println(f);
+            System.out.println(g);
+            System.out.println(h);
+            sayE();
+            sayF();
+            sayG();
+            sayH();
+        }
+    }
+    public static class InnerStatic {
+        /*--------------Methods--------------*/
+        public static void callStatic() {
             System.out.println();
-            sayA(); //access to public method
-            sayB(); //access to protected method
-            sayC(); //access to private method
-            sayD(); //access to package method
+            System.out.println("The INNER CLASS has private static, default static, "
+                    + "protected static and public static access to the OUTER CLASS");
+            System.out.println(e);
+            System.out.println(f);
+            System.out.println(g);
+            System.out.println(h);
+            sayE();
+            sayF();
+            sayG();
+            sayH();
         }
     }
 }
