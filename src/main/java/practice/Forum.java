82e585b21ff5d4d01d4555405cc943b54ff37c32
Serhii Kondriuchok <kondrserg@gmail.com>
2025-11-04 23:52:53 +0200

    Add new classes

--- src/main/java/practice/Forum.java
+++ src/main/java/practice/Forum.java
@@ -2,7 +2,10 @@ package practice;
 
 public class Forum {
     public static void main(String[] args) {
-        Person p = new Male();
+        listNames(new Male());
+        listNames(new Female());
+    }
+    private static void listNames(Person p) {
         for (Name n : p.getNames()) {
             n.sayName();
         }
--- src/main/java/practice/Male.java
+++ src/main/java/practice/Male.java
@@ -30,3 +30,35 @@ public class Male implements Person {
         return names;
     }
 }
+
+//Object: Forum
+//    Interface objects: -
+//    Local objects: Person, Name
+
+//Object: Male
+//    Interface objects: Person
+//    Local objects: Name[]
+
+//Object: Female
+//    Interface objects: Person
+//    Local objects: Name[]
+
+//Object: Male.RareName
+//    Interface objects: Name
+//    Local objects: -
+
+//Object: Male.SimpleName
+//    Interface objects: Name
+//    Local objects: -
+
+//Object: Male.ExoticName
+//    Interface objects: Name
+//    Local objects: -
+
+//Object: Name
+//    Interface objects: -
+//    Local objects: -
+
+//Object: Person
+//    Interface objects: -
+//    Local objects: -
