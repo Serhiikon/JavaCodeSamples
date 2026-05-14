8bc0170f10a6dfad8f07d8bc1d799b302b664b04
Serhii Kondriuchok <kondrserg@gmail.com>
2026-05-14 18:31:47 +0100

    Delete some stuff

--- src/main/java/basepackage/JavaCodeSamples.java
+++
@@ -1,8 +0,0 @@
-package basepackage;
-
-public class JavaCodeSamples {
-    /*--------------Methods--------------*/
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
